8452b1434f43ab057c4fb97688b99e50dfb5815b
DZero <dzd624175217@gmail.com>
2020-07-10 17:27:50 +0800

    添加Spring Boot Admin 的示例

---
+++ spring-boot-admin-client/src/main/java/com/dzero/springbootadminclient/SpringBootAdminClientApplication.java
@@ -0,0 +1,16 @@
+package com.dzero.springbootadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dzero
+ */
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class, args);
+    }
+
+}
