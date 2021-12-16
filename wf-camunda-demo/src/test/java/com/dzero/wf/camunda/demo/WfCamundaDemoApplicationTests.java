7e0a09d98eed3d89041be1602900a31fdfcadc3a
DZero <dzd624175217@gmail.com>
2021-12-16 16:14:36 +0800

    add test

--- wf-camunda-demo/src/test/java/com/dzero/wf/camunda/demo/WfCamundaDemoApplicationTests.java
+++ wf-camunda-demo/src/test/java/com/dzero/wf/camunda/demo/WfCamundaDemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.dzero.wf.camunda.demo;
 
+import com.dzero.wf.camunda.demo.service.CamundaService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WfCamundaDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private CamundaService camundaService;
 
+    @Test
+    void startServiceTaskDemo(){
+        camundaService.startProcessInstanceByKey("ServiceTaskDemo_001");
+    }
 }
