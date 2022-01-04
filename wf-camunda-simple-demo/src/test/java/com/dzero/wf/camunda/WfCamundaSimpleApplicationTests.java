9fa14330e4534f8b2e83f44c8a72a2db3e63dccc
DZero <dzd624175217@gmail.com>
2022-01-04 20:04:54 +0800

    add camunda simple demo

---
+++ wf-camunda-simple-demo/src/test/java/com/dzero/wf/camunda/WfCamundaSimpleApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dzero.wf.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfCamundaSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
