b0ee464136ea654640331020d10a8b450c7480f9
DZero <dzd624175217@gmail.com>
2022-02-28 17:43:13 +0800

    [新增]更新工作流监听器

--- wf-camunda-demo/src/main/java/com/dzero/wf/camunda/demo/listener/MyListener.java
+++ wf-camunda-demo/src/main/java/com/dzero/wf/camunda/demo/listener/MyListener.java
@@ -3,6 +3,8 @@ package com.dzero.wf.camunda.demo.listener;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.impl.history.event.HistoricActivityInstanceEventEntity;
+import org.camunda.bpm.engine.impl.history.event.HistoricProcessInstanceEventEntity;
 import org.camunda.bpm.engine.impl.history.event.HistoryEvent;
 import org.camunda.bpm.spring.boot.starter.event.ExecutionEvent;
 import org.camunda.bpm.spring.boot.starter.event.TaskEvent;
@@ -18,46 +20,64 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class MyListener {
 
-    @EventListener(condition = "#event.eventName=='create'")
-    public void onTaskCreatedEvent(TaskEvent event) {
-        // handle immutable task event
-        log.info("handle task event {} for task id {}", event.getEventName(), event.getId());
-    }
-
-    @EventListener(condition = "#event.eventName=='complete'")
-    public void onTaskCompletedEvent(TaskEvent event) {
-        // handle immutable task event
-        log.info("handle task event {} for task id {}", event.getEventName(), event.getId());
-    }
+//    @EventListener(condition = "#event.eventName=='create'")
+//    public void onTaskCreatedEvent(TaskEvent event) {
+//        // handle immutable task event
+//        log.info("handle task event {} for task id {}", event.getEventName(), event.getId());
+//    }
+//
+//    @EventListener(condition = "#event.eventName=='complete'")
+//    public void onTaskCompletedEvent(TaskEvent event) {
+//        // handle immutable task event
+//        log.info("handle task event {} for task id {}", event.getEventName(), event.getId());
+//    }
 
     @EventListener
     public void onTaskEvent(DelegateTask taskDelegate) {
         // handle mutable task
-        log.info("onTaskEvent-DelegateTask" + taskDelegate.getProcessDefinitionId());
+        log.info("========= DelegateTask =========" + taskDelegate.getEventName() + "====" + taskDelegate.getProcessInstanceId());
     }
 
     @EventListener
     public void onTaskEvent(TaskEvent taskEvent) {
         // handle immutable task event
-        log.info("onTaskEvent-TaskEvent" + taskEvent.getProcessDefinitionId());
+        log.info("========= TaskEvent =========" + taskEvent.getEventName() + "====" + taskEvent.getProcessInstanceId());
     }
 
     @EventListener
     public void onExecutionEvent(DelegateExecution executionDelegate) {
         // handle mutable execution event
-        log.info("onExecutionEvent-DelegateExecution" + executionDelegate.getProcessDefinitionId());
+        log.info("========= DelegateExecution =========" + executionDelegate.getEventName() + "====" + executionDelegate.getProcessInstanceId());
     }
 
     @EventListener
     public void onExecutionEvent(ExecutionEvent executionEvent) {
         // handle immutable execution event
-        log.info("onExecutionEvent-ExecutionEvent" + executionEvent.getProcessDefinitionId());
+        log.info("========= ExecutionEvent =========" + executionEvent.getEventName() + "====" + executionEvent.getProcessInstanceId());
     }
 
     @EventListener
     public void onHistoryEvent(HistoryEvent historyEvent) {
-        // handle history event
-        log.info("onHistoryEvent-HistoryEvent" + historyEvent.getProcessDefinitionId());
+        if (historyEvent instanceof HistoricProcessInstanceEventEntity) {
+            HistoricProcessInstanceEventEntity event = (HistoricProcessInstanceEventEntity) historyEvent;
+            switch (event.getState()) {
+                case "ACTIVE": {
+                    log.info("========= 流程激活 ===== " + event.getEventType() + " ====== " + event.getProcessInstanceId());
+                }
+                break;
+                case "COMPLETED": {
+                    log.info("========= 流程完成 ===== " + event.getEventType() + " ====== " + event.getProcessInstanceId());
+                }
+                break;
+                default:
+                    break;
+            }
+        } else if (historyEvent instanceof HistoricActivityInstanceEventEntity) {
+            HistoricActivityInstanceEventEntity activityInstanceEvent = (HistoricActivityInstanceEventEntity)historyEvent;
+            if ("userTask".equals(activityInstanceEvent.getActivityType()) && "start".equals(activityInstanceEvent.getEventType())) {
+                log.info("historyEvent");
+            }
+        }
     }
 
 }
